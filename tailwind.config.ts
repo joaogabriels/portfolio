f2b727c977ebc4279536918160666bfb49217cc9
joaogabriels <joaogabrielsevangelista@gmail.com>
2025-01-27 21:12:19 -0300

    feat(app): adiciona base do app

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,27 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+
+        'base-50': 'var(--color-base-50)',
+        'base-100': 'var(--color-base-100)',
+        'base-200': 'var(--color-base-200)',
+        'base-300': 'var(--color-base-300)',
+        'base-400': 'var(--color-base-400)',
+        'base-500': 'var(--color-base-500)',
+        'base-600': 'var(--color-base-600)',
+        'base-700': 'var(--color-base-700)',
+        'base-800': 'var(--color-base-800)',
+        'base-900': 'var(--color-base-900)',
+        'base-950': 'var(--color-base-950)',
       },
+
+      fontFamily: {
+        courier_prime: ["var(--font-courier-prime)"]
+      },
+
+      boxShadow: {
+        card: '0.75vmin 0.75vmin var(--color-base-950);'
+      }
     },
   },
   plugins: [],
