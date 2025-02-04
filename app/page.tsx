519584c3b2b2a5ea396d5be1aa549f559cf89a52
joaogabriels <joaogabrielsevangelista@gmail.com>
2025-02-04 20:54:09 -0300

    refactor(page): altera animação de blobs

--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,11 @@ export default function Home() {
   return (
     <main>
       <div className="relative w-full h-full overflow-hidden">
-        <div className="absolute top-0 left-10 size-[200px] bg-red-500 rounded-full z-0 blur-2xl animate-blob opacity-25 mix-blend-soft-light animation-delay-1" />
-        <div className="absolute top-0 right-10 size-[1000px] bg-purple-500 rounded-full z-0 blur-2xl animate-blob opacity-25 mix-blend-soft-light animation-delay-2" />
-        <div className="absolute top-2/4 right-10 size-[1000px] bg-yellow-500 rounded-full z-0 blur-2xl animate-blob opacity-25 mix-blend-soft-light animation-delay-1" />
-        <div className="absolute top-2/4 left-10 size-[1000px] bg-purple-500 rounded-full z-0 blur-2xl animate-blob opacity-25 mix-blend-soft-light animation-delay-2" />
-        <div className="absolute bottom-0 right-10 size-[1000px] bg-red-500 rounded-full z-0 blur-2xl animate-blob opacity-25 mix-blend-soft-light animation-delay-1" />
+        <div className="absolute top-0 left-10 size-[1000px] bg-red-500 rounded-full z-0 blur-2xl animate-blob opacity-35 mix-blend-soft-light animation-delay-1" />
+        <div className="absolute top-0 right-10 size-[1000px] bg-purple-500 rounded-full z-0 blur-2xl animate-blob opacity-50 mix-blend-soft-light animation-delay-2" />
+        <div className="absolute top-2/4 right-10 size-[1000px] bg-yellow-500 rounded-full z-0 blur-2xl animate-blob opacity-50 mix-blend-soft-light animation-delay-1" />
+        <div className="absolute top-2/4 left-10 size-[1000px] bg-purple-500 rounded-full z-0 blur-2xl animate-blob opacity-50 mix-blend-soft-light animation-delay-2" />
+        <div className="absolute bottom-0 right-10 size-[1000px] bg-red-500 rounded-full z-0 blur-2xl animate-blob opacity-50 mix-blend-soft-light animation-delay-1" />
 
         <div className="relative container mx-auto my-10 p-10 border border-neutral-800 rounded-xl bg-neutral-950/50 backdrop-blur-sm w-screen max-md:px-4 max-md:my-4">
           <section className="h-screen w-full flex items-center justify-between gap-4 max-lg:flex-col max-md:items-start max-md:justify-center">
