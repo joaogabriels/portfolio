35acfcfa3cb706dd1be71c16653cac1d8fcbd262
joaogabriels <joaogabrielsevangelista@gmail.com>
2025-02-04 22:26:01 -0300

    feat(shadcnui): adiciona biblioteca de estilos

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,36 @@
 import Link from "next/link";
 
-import { LucideGithub, LucideLinkedin } from "lucide-react";
-import Image from "next/image";
+import {
+  Laugh,
+  LucideGithub,
+  LucideLinkedin,
+  TerminalSquare,
+} from "lucide-react";
+import { Badge } from "@/components/ui/badge";
 
 export default function Home() {
+  const WORK_TECHNOLOGIES = [
+    "JavaScript",
+    "TypeScript",
+    "React",
+    "HTML",
+    "CSS",
+    "Tailwind",
+    "NextJS",
+    "React Testing Library",
+    "Jest",
+    "Vitest",
+    "NodeJS",
+    "Express",
+    "Vercel",
+    "Docker",
+    "Git",
+    "Jira",
+    "Wordpress",
+  ];
+
+  const HOBBY_TECHNOLOGIES = ["Python", "C++", "AWS", "Firebase", "Figma"];
+
   return (
     <main>
       <div className="relative w-full h-full overflow-hidden">
@@ -13,15 +40,16 @@ export default function Home() {
         <div className="absolute top-2/4 left-10 size-[1000px] bg-purple-500 rounded-full z-0 blur-2xl animate-blob opacity-50 mix-blend-soft-light animation-delay-2" />
         <div className="absolute bottom-0 right-10 size-[1000px] bg-red-500 rounded-full z-0 blur-2xl animate-blob opacity-50 mix-blend-soft-light animation-delay-1" />
 
-        <div className="relative container mx-auto my-10 p-10 border border-neutral-800 rounded-xl bg-neutral-950/50 backdrop-blur-sm w-screen max-md:px-4 max-md:my-4">
+        <div className="relative container mx-auto my-10 p-10 border border-neutral-800 rounded-xl bg-neutral-950/50 backdrop-blur-sm w-screen max-md:px-4 max-md:my-4 space-y-4">
           <section className="h-screen w-full flex items-center justify-between gap-4 max-lg:flex-col max-md:items-start max-md:justify-center">
             <div className="space-y-4">
-              <h1 className="max-lg:text-3xl max-lg:max-w-72 text-6xl max-w-xl font-semibold leading-tight">
-                Olá! Me chamo João Evangelista.
+              <h1 className="max-lg:text-3xl max-lg:max-w-full text-6xl max-w-screen-sm font-semibold leading-tight">
+                Olá! Me chamo João Evangelista
+                <span className="gradient-text animate-gradient-text">.</span>
               </h1>
 
-              <h2 className="max-lg:text-lg max-lg:max-w-96 text-xl text-base-500 max-w-lg">
-                Sou um{" "}
+              <h2 className="max-lg:text-lg max-lg:max-w-full text-xl text-base-500 max-w-lg">
+                Sou{" "}
                 <span className="gradient-text animate-gradient-text">
                   desenvolvedor de software
                 </span>
@@ -33,51 +61,73 @@ export default function Home() {
               <Link
                 href="https://github.com/joaogabriels"
                 target="_blank"
-                className="hover:text-base-200 transition-colors"
+                className="hover:text-orange-500 transition-colors"
               >
-                <LucideGithub className="w-6 h-6" />
+                <LucideGithub className="size-5" />
               </Link>
 
               <Link
                 href="https://linkedin.com/in/joaogabrielse"
                 target="_blank"
-                className="hover:text-base-200 transition-colors"
+                className="hover:text-orange-500 transition-colors"
               >
-                <LucideLinkedin className="w-6 h-6" />
+                <LucideLinkedin className="size-5" />
               </Link>
             </div>
           </section>
 
-          <section className="h-full w-full flex justify-between items-start gap-4 max-lg:flex-col max-lg:items-center">
+          <section className="grid grid-cols-2 gap-4 max-lg:grid-cols-1">
             <div className="space-y-4">
               <h2 className="text-xl text-base-500">Sobre mim</h2>
 
-              <div className="space-y-4 text-xl w-4/5 max-md:text-md max-md:w-full">
-                <p>
-                  Desenvolvedor de graduando em Sistemas de Informação (último
-                  ano).
-                </p>
+              <div className="space-y-4 text-xl max-md:text-md  max-md:w-full">
+                <p>Sou graduando em Sistemas de Informação (último ano).</p>
 
                 <p>
-                  Atuo profissionalmente no desenvolvimento web desde 2022, onde já
-                  tive a oportunidade de trabalhar com diversas tecnologias e
-                  me conectar com diversas pessoas.
+                  Atuo profissionalmente no desenvolvimento web desde 2022,
+                  tendo trabalhado com diversas tecnologias e me conectado com
+                  muitas pessoas.
                 </p>
 
-                <p>
-                  Vamos conversar!
-                </p>
+                <p>Vamos conversar!</p>
               </div>
             </div>
 
-            <Image
-              src="/joaoevangelista.jpg"
-              alt="João Evangelista"
-              width={400}
-              height={400}
-              className="rounded-xl"
-              quality={100}
-            />
+            <div className="space-y-4">
+              <div className="space-y-4">
+                <div className="flex justify-start items-center gap-4">
+                  <TerminalSquare className="size-6 text-orange-500" />
+
+                  <h3>Utilizado no trabalho</h3>
+                </div>
+
+                <div className="flex gap-4 flex-wrap max-w-md">
+                  {WORK_TECHNOLOGIES.map((technology) => (
+                    <Badge key={technology}>{technology}</Badge>
+                  ))}
+                </div>
+              </div>
+
+              <div className="space-y-4">
+                <div className="flex justify-start items-center gap-4">
+                  <Laugh className="size-6 text-orange-500" />
+
+                  <h3>Utilizado por hobby</h3>
+                </div>
+
+                <div className="flex gap-4 flex-wrap max-w-1/2">
+                  {HOBBY_TECHNOLOGIES.map((technology) => (
+                    <Badge key={technology}>{technology}</Badge>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </section>
+
+          <section className="h-full w-full flex justify-between items-start gap-4 max-lg:flex-col max-lg:items-start">
+            <div className="space-y-4">
+              <h2 className="text-xl text-base-500">Experiências</h2>
+            </div>
           </section>
         </div>
       </div>
---
+++ components/ui/badge.tsx
@@ -0,0 +1,31 @@
+import * as React from "react"
+import { cva, type VariantProps } from "class-variance-authority"
+
+import { cn } from "@/lib/utils"
+
+const badgeVariants = cva(
+  "inline-flex items-center rounded-full border px-2.5 pb-1 pt-2 text-xs font-semibold transition-colors leading-2 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2",
+  {
+    variants: {
+      variant: {
+        default: "border-none bg-neutral-900",
+        outline: "text-foreground",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+    },
+  }
+)
+
+export interface BadgeProps
+  extends React.HTMLAttributes<HTMLDivElement>,
+    VariantProps<typeof badgeVariants> {}
+
+function Badge({ className, variant, ...props }: BadgeProps) {
+  return (
+    <div className={cn(badgeVariants({ variant }), className)} {...props} />
+  )
+}
+
+export { Badge, badgeVariants }
